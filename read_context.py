2ea52b389e91a694591a3ad595acb11f0a952241
김충환 <hello.kch@gmail.com>
2023-03-23 22:35:47 +0900

    수정

--- read_context.py
+++ read_context.py
@@ -13,17 +13,17 @@ from bs4 import BeautifulSoup
 import re
 
 # csv 파일 읽기
-df = pd.read_csv('read_url_fin.csv',index_col=0)
+df = pd.read_csv('project_2/read_url_fin.csv',index_col=0)
 df.info()
 # 각 url링크에 접속하고 웹페이지 내용 파싱하여 텍스트 저장
 texts = []
 for url in df['url']:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    dl_con = soup.find_all('dl','qnaLists')
-    for con in dl_con:
-        text = con.text.split('질문Q')
-        for i in range(1, len(text)):
+    dl_con = soup.find('dl','qnaLists')
+    text = dl_con.text.split('질문Q')
+
+    for i in range(1, len(text)):
             qna = text[i].split('보기\n\n\n답변')
             question = qna[0].replace('\n', '')
             answer = qna[1].replace('\n', '').replace('\r', '')
@@ -34,10 +34,11 @@ for url in df['url']:
                           question,\
                           answer])
 
+
 # DataFrame으로 변환
 qna_df = pd.DataFrame(texts, columns=['기업명', '산업분류', '질문', '답변'])
 
 # csv 파일로 저장
-qna_df.to_csv('qna.csv', index=False, encoding='utf-8-sig')
+qna_df.to_csv('qna.csv', index=False, encoding='utf-8')
 
 
