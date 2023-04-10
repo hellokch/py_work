7b1fac22a54f4bd08eb038852f417f0dbdec9b4d
김충환 <hello.kch@gmail.com>
2023-04-10 21:29:55 +0900

    0410 0930

--- jobara/Load.py
+++ jobara/Load.py
@@ -1,40 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 10 17:48:47 2023
-
-@author: 김충환
-"""
-
-# AIzaSyBw7dSCaWXCCqi4zG-TH8bjSHxpVEJ2CAw
-
-# 360183029918-bni9lufkerv6h8teknk7p1o5u34j3nrn.apps.googleusercontent.com
-# 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
-
-
 # pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
 
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+import pandas as pd
 
 # Google API credentials 파일 경로
-creds_path = 'path/to/credentials.json'
+creds_path = 'path/to/auth.json'
 token_path = 'path/to/token.json'
 
+# 사용자 인증 흐름 객체 생성
+flow = InstalledAppFlow.from_client_secrets_file(
+    creds_path,
+    scopes=['https://www.googleapis.com/auth/drive']
+)
+
+# 사용자 인증 진행
+creds = flow.run_local_server(port=8080)
+
+# 인증 정보 저장
+with open(token_path, 'w') as token_file:
+    token_file.write(creds.to_json())
+
 # 구글 API 권한 인증 및 빌드
-creds = Credentials.from_authorized_user_file(token_path)
 service = build('drive', 'v3', credentials=creds)
 
-# 파일 ID와 파일명
-file_id = 'your_file_id'
-file_name = 'your_file_name'
+# 파일 ID
+file_id = '1paHKrII_xr09mDFSF_qKEd0_-sq7ng2f'
+
+# 파일 속성 표시
+file = service.files().get(fileId=file_id).execute()
+print(file)
+
 
 try:
+    # 파일 정보 가져오기
+    file = service.files().get(fileId=file_id).execute()
+
     # 파일 다운로드
-    file = service.files().get_media(fileId=file_id).execute()
-    with open(file_name, 'wb') as f:
-        f.write(file)
-    print(f'File "{file_name}" has been downloaded.')
+    file_content = service.files().get_media(fileId=file_id).execute()
+
+    # 바이너리 형식으로 다운로드한 파일 컨텐츠를 문자열로 변환
+    content = file_content.decode('utf-8')
+
+    # Pandas DataFrame으로 변환
+    df = pd.read_csv(content)
+    print(df.head())
 except HttpError as error:
     print(f'An error occurred: {error}')
-    file = None
+
